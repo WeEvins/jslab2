d3b514fa5648b0d5d145a8f5d7af42b227cde50c
Warryn Evins <W.Hatmaker@yahoo.com>
2018-04-18 18:56:56 -0500

    Styles
    did the css whoop whoop

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let friends = ['Juleca', 'Marinette', 'Alya', 'Mylene', 'Rose']
 
 for (let f = 0; f < friends.length; f++) {
     for (let i = 99; i >= 1; i--) {
-        console.log(`${i} lines of code in the file, ${i} lines of code; ${friends[f]} strikes one out, clears it all out. ${i-1} lines of code on the wall`)
+        let currentPlural = (i > 1) ? 'lines' : 'line'
+        let nextPlural = (i === 2) ? 'line' : 'lines'
+        
+        console.log(`${i} ${currentPlural} of code in the file, ${i} ${currentPlural} of code; ${friends[f]} strikes one out, clears it all out. ${i-1} ${nextPlural} of code on the wall`)
     }
 }
